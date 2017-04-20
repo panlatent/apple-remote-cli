7dc13e3d893948010f8bc6a62a24e275cea1b3df
panlatent <panlatent@gmail.com>
2017-04-20 15:10:30 +0800

    Update IntervalTimer control

--- src/Player/Timer/IntervalTimer.php
+++ src/Player/Timer/IntervalTimer.php
@@ -13,17 +13,27 @@ abstract class IntervalTimer extends TimerAbstract
 {
     protected $dispatcher;
 
+    protected $running;
+
     public function __construct(Dispatcher $dispatcher, $delay)
     {
         $this->dispatcher = $dispatcher;
         $this->delay = $delay;
+        $this->running = true;
     }
 
     final public function run()
     {
         $this->interval();
-        $this->dispatcher->addTimer($this);
+        if ($this->running) {
+            $this->dispatcher->addTimer($this);
+        }
     }
 
     abstract public function interval();
+
+    public function clear()
+    {
+        $this->running = false;
+    }
 }
