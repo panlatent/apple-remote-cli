7634c88c894fc3b2ed78c0fd99c500088b45faa1
panlatent <panlatent@gmail.com>
2017-05-02 20:58:10 +0800

    Add configure file support and update command name

--- src/Commands/Command.php
+++ src/Commands/Command.php
@@ -10,12 +10,40 @@
 namespace Panlatent\AppleRemoteCli\Commands;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 
 class Command extends \Symfony\Component\Console\Command\Command
 {
+    protected $config = [];
+
+    protected function configure()
+    {
+        $this->addOption('auth', null, InputOption::VALUE_OPTIONAL, 'authorization code');
+        $this->addOption('host', null, InputOption::VALUE_OPTIONAL, 'server host', '127.0.0.1');
+        $this->addOption('port', null, InputOption::VALUE_OPTIONAL, 'server port', '3689');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->loadConfigureFile();
+        if ($input->getOption('auth')) {
+            $this->config['connect']['auth'] = $input->getOption('auth');
+        }
+        if ($input->getOption('host')) {
+            $this->config['connect']['host'] = $input->getOption('host');
+        }
+        if ($input->getOption('port')) {
+            $this->config['connect']['port'] = $input->getOption('port');
+        }
+    }
 
+    protected function loadConfigureFile()
+    {
+        $filename = getenv('HOME') . DIRECTORY_SEPARATOR . '.apple-remote.yml';
+        if (is_file($filename)) {
+            $this->config = Yaml::parse(file_get_contents($filename));
+        }
     }
 }
--- src/Commands/ControlCommand.php
+++ src/Commands/ControlCommand.php
@@ -12,7 +12,6 @@ namespace Panlatent\AppleRemoteCli\Commands;
 use Panlatent\AppleRemoteCli\PlayControl;
 use Panlatent\AppleRemoteCli\RemoteClient;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class ControlCommand extends Command
@@ -25,17 +24,13 @@ abstract class ControlCommand extends Command
     protected function configure()
     {
         parent::configure();
-        $this->addOption('auth', null, InputOption::VALUE_REQUIRED, 'authorization code');
-        $this->addOption('host', null, InputOption::VALUE_OPTIONAL, 'server host', '127.0.0.1');
-        $this->addOption('port', null, InputOption::VALUE_OPTIONAL, 'server port', '3689');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         parent::execute($input, $output);
-        $authorizationCode = $input->getOption('auth');
-        $remote = new RemoteClient($input->getOption('host'), $input->getOption('port'));
-        $remote->login($authorizationCode);
+        $remote = new RemoteClient($this->config['connect']['host'], $this->config['connect']['port']);
+        $remote->login($this->config['connect']['auth']);
         $this->control = new PlayControl($remote);
     }
 }
